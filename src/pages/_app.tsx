20a1c6fcc5e3f38f8836dd34ff301806cc1d84e5
NicholasWM <nicholaswesleymazzei@gmail.com>
2022-04-01 09:16:29 -0300

    add: Default Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import { ChakraProvider, Flex } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
@@ -5,7 +6,12 @@ import { theme } from '../styles/theme'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Flex maxWidth={'1112px'} m="auto" flexDir="column" justifyContent="center" alignItems="center">
+      <Head>
+        <title>Vlow</title>
+        <meta name="description" content="Challenge CRUD" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Flex maxWidth={'1112px'} px="14px" m="auto" flexDir="column" justifyContent="center" alignItems="center">
         <Component {...pageProps} />
       </Flex>
     </ChakraProvider>
