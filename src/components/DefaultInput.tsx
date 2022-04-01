7007149f51942e1ff4cb52e25661356671f05a56
NicholasWM <nicholaswesleymazzei@gmail.com>
2022-03-31 22:42:43 -0300

    add: masks and unform to DefaultInput

--- src/components/DefaultInput.tsx
+++ src/components/DefaultInput.tsx
@@ -1,28 +1,67 @@
-import { Input } from "@chakra-ui/react";
-
+import { Text, Input } from "@chakra-ui/react";
+import { useField } from "@unform/core";
+import { useRef, useEffect } from "react";
+import InputMask from 'react-input-mask'
 type colors = '#494d5d'
 
-interface  IDefaultInputProps{
-    type?: 'text'| 'password'| 'email',
+interface IDefaultInputProps {
+    type?: 'text' | 'password' | 'email',
     color?: colors,
     id: string,
     placeholder: string,
+    name: string,
+    mask?: string,
+    onChange?: () => void
 }
 export function DefaultInput({
-    type='text',
-    color='#494d5d',
+    type = 'text',
+    color = '#494d5d',
     id,
     placeholder,
- }:IDefaultInputProps){
+    name,
+    onChange,
+    ...rest
+}: IDefaultInputProps) {
+    const inputRef = useRef(null)
+    const { fieldName, defaultValue, registerField, error } = useField(name)
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef,
+            getValue: ref => {
+                return ref.current.value
+            },
+            setValue: (ref, value) => {
+                ref.current.value = value
+            },
+            clearValue: ref => {
+                ref.current.value = ''
+            },
+        })
+    }, [fieldName, registerField])
     return (
-        <Input
-            focusBorderColor="#015eff" 
-            fontSize='16px' 
-            size='lg'
-            color={color} 
-            type={type}
-            id={id} 
-            placeholder={placeholder}
-        />
+        <>
+            <Input
+                ref={inputRef}
+                name={name}
+                as={InputMask}
+                focusBorderColor="#015eff"
+                style={error && {borderColor:'red'}}
+                fontSize='16px'
+                size='lg'
+                color={color}
+                type={type}
+                id={id}
+                errorBorderColor='red'
+                onChange={onChange}
+                placeholder={placeholder}
+                {...rest}
+            />
+            {error && (
+                <span>
+                    <Text fontSize={"0.8rem"} color='red'>{error}</Text>
+                </span>
+            )}
+        </>
     )
 } 
